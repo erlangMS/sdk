dd48477133f2be3185b20b173f661b144b76abb5
evertonagilar <evertonagilar@gmail.com>
2015-10-09 08:01:58 -0300

    Implementacao classe EmsValidation

---
+++ java/ems_java/src/br/erlangms/EmsValidation.java
@@ -0,0 +1,32 @@
+package br.erlangms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmsValidation extends RuntimeException {
+	private static final long serialVersionUID = -8316509235178192483L;
+	private List<String> errors;
+	
+	public EmsValidation () {
+		errors = new ArrayList<>();
+	}
+	
+	public EmsValidation (String e) {
+		this ();
+		errors.add(e);
+	}
+	
+	
+	public EmsValidation (List <String> l) {
+		errors = l;
+	}
+	
+	public void addError(String error) {
+		errors.add(error);
+	}
+	
+	public List<String> getErrors () {
+		return errors;
+	}
+	
+}
