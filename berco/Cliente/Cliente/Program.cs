f1e36cfd2c20a67569c0f71df825ec328a2d9aac
evertonagilar <evertonagilar@gmail.com>
2016-05-20 20:31:42 -0300

    Cliente funcionando

---
+++ berco/Cliente/Cliente/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Erlang.NET;
+using log4net;
+using log4net.Config;
+
+namespace Cliente
+{
+	class MainClass
+	{
+		// Define a static logger variable so that it references the
+		// Logger instance named "MyApp".
+		private static readonly ILog log = LogManager.GetLogger(typeof(MainClass));
+
+		public static void Main (string[] args)
+		{
+			// Set up a simple configuration that logs on the console.
+			BasicConfigurator.Configure();
+
+			log.Info("Entering application.");
+
+			System.Console.WriteLine("Cliente CSharp ErlangMS");
+
+			OtpSelf cNode = new OtpSelf("clientnode", "aula");
+			OtpPeer sNode = new OtpPeer("no1@puebla");
+
+			OtpConnection connection = cNode.connect(sNode);
+
+			OtpErlangObject[] msg = new OtpErlangObject[] { 
+				new OtpErlangLong(2), new OtpErlangLong(5)
+			}; 
+
+			connection.sendRPC("mathserver", "multiply", msg);
+
+			OtpErlangLong sum = (OtpErlangLong)connection.receiveRPC();
+			Console.WriteLine("Return Value:" + sum.ToString());
+
+			log.Info("Exiting application.");
+
+		}
+	}
+}
--- berco/mathserver.erl
+++ berco/mathserver.erl
@@ -2,4 +2,5 @@
 -compile(export_all).
  
 multiply(First, Second) ->
-First * Second.
+	io:format("passei aqui: ~p :  ~p!\n\n", [First, Second]),
+	First * Second.
