33aa48e4c9ba16848c3ac94558a2f30177aff356
Willian Pinho <willjrpp@gmail.com>
2016-07-01 12:50:57 -0300

    Berço Thread

---
+++ berco/Thread/ThreadBerco/ThreadBerco/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+            
+namespace ThreadBerco
+{
+	class MainClass
+	{
+
+
+		public static void Main(string[] args)
+		{
+			Worker workerObject = new Worker();
+			Thread workerThread = new Thread(workerObject.DoWork);
+
+			workerThread.Start();
+
+			while (!workerThread.IsAlive) ;
+			Thread.Sleep(1);
+
+			workerObject.RequestStop();
+
+			workerThread.Join();
+
+			Console.WriteLine("Main Thread: Worker thread terminou.");
+
+
+		}
+
+
+	}
+}
---
+++ berco/Thread/ThreadBerco/ThreadBerco/Worker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadBerco
+{
+	public class Worker
+	{
+
+		private volatile bool _shouldStop;
+
+
+		public Worker()
+		{
+		}
+
+		public void DoWork()
+		{
+			while (!_shouldStop)
+			{
+				Console.WriteLine("Worker thread: Iniciando...");
+			}
+			Console.WriteLine("worker thread: finalizado com sucesso");
+		}
+
+		public void RequestStop()
+		{
+			_shouldStop = true;
+		}
+	}
+}
