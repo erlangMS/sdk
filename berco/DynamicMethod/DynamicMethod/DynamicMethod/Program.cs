024227be03de3598c1709c34902b4691948d6b8f
Willian Pinho <willjrpp@gmail.com>
2016-07-01 16:53:58 -0300

    Berco Chama Metodo

---
+++ berco/DynamicMethod/DynamicMethod/DynamicMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+
+namespace DynamicMethod
+{
+	class MainClass
+	{
+
+		public static void Main(string[] args)
+		{
+			Type tipo = typeof(MainClass);
+			MethodInfo m = tipo.GetMethod("HelloWorld");
+			Object obj = new MainClass();
+
+			Object[] parameters = { "teste" }; 
+	
+			m.Invoke(obj, parameters);
+
+		}
+
+		public void HelloWorld(String msg)
+		{
+			
+			Console.WriteLine(msg);
+
+		}
+			
+
+	}
+}
