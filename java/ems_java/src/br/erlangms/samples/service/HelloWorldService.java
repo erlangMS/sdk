5d1c8f38a8725cdadbeec13726b1e774103c4d48
evertonagilar <evertonagilar@gmail.com>
2017-02-20 11:55:32 -0300

    sdk improve

--- java/ems_java/src/br/erlangms/samples/service/HelloWorldService.java
+++ java/ems_java/src/br/erlangms/samples/service/HelloWorldService.java
@@ -4,7 +4,6 @@ import javax.ejb.Singleton;
 import javax.ejb.Startup;
 
 import br.erlangms.EmsServiceFacade;
-import br.erlangms.EmsUtil;
 import br.erlangms.IEmsRequest;
 
 @Singleton
@@ -16,10 +15,6 @@ public class HelloWorldService extends EmsServiceFacade {
 	}
 
 	public String helloWorld(IEmsRequest request) {
-		
-		String[] anexos = new String[1];
-		anexos[0] = "/home/everton/desenvolvimento/erlangms/ems-bus/build.sh";
-		EmsUtil.sendTextMail("evertonagilar@gmail.com", "isso é um teste", "contedo do email", anexos);
 		return "{\"message\":\"Hello World Java!!!\"}";
 	}
 	
