c1933e9644a4890baa1ff21b871fb4a128843aa1
Everton de Vargas Agilar <evertonagilar@ufsm.br>
2019-07-11 10:30:59 -0300

    Melhorado o método strToEnum para o barramento do SEI

--- java/ems_java/src/br/unb/erlangms/rest/util/EnumUtils.java
+++ java/ems_java/src/br/unb/erlangms/rest/util/EnumUtils.java
@@ -1,8 +1,9 @@
 package br.unb.erlangms.rest.util;
 
+import com.google.gson.annotations.SerializedName;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javax.xml.bind.annotation.XmlEnumValue;
 
 /**
  * <p>Funções úteis para trabalhar com enums</p>
@@ -19,7 +20,7 @@ public final class EnumUtils {
     * @return enumeração
     * @author Everton de Vargas Agilar
     */
-   public static Enum<?> intToEnum(int value, @SuppressWarnings("rawtypes") final Class<Enum> clazz) {
+   public static Enum<?> intToEnum(int value, final Class<Enum> clazz) {
            if (clazz != null){
                    if (value >= 0){
                            for(Enum<?> t : clazz.getEnumConstants()) {
@@ -41,7 +42,7 @@ public final class EnumUtils {
     * @return enumeração
     * @author Everton de Vargas Agilar
     */
-   public static Enum<?> strToEnum(final String value, @SuppressWarnings("rawtypes") final Class<Enum> clazz) {
+   public static Enum<?> strToEnum(final String value, final Class<Enum> clazz) {
            if (value != null && !value.isEmpty() && clazz != null){
                    for(Enum<?> t : clazz.getEnumConstants()) {
                        if (t instanceof StringEnum){
@@ -52,16 +53,23 @@ public final class EnumUtils {
                         }else{
                            try {
                                Field fld = t.getClass().getField(t.name());
-                               Object value2 = fld.get("value");
-                               System.out.println(fld.toString());
-                           } catch (NoSuchFieldException ex) {
-                               Logger.getLogger(EnumUtils.class.getName()).log(Level.SEVERE, null, ex);
-                           } catch (SecurityException ex) {
-                               Logger.getLogger(EnumUtils.class.getName()).log(Level.SEVERE, null, ex);
-                           } catch (IllegalArgumentException ex) {
-                               Logger.getLogger(EnumUtils.class.getName()).log(Level.SEVERE, null, ex);
-                           } catch (IllegalAccessException ex) {
-                               Logger.getLogger(EnumUtils.class.getName()).log(Level.SEVERE, null, ex);
+                               // Se tiver anotação, vamos procurar nas anotações também
+                               Annotation[] annotations = fld.getAnnotations();
+                               for (Annotation ann : annotations){
+                                   if (ann instanceof XmlEnumValue){
+                                       XmlEnumValue xmlEnumValue = (XmlEnumValue) ann;
+                                       if (xmlEnumValue.value().equalsIgnoreCase(value)){
+                                           return t;
+                                       }
+                                   }else if (ann instanceof SerializedName){
+                                       SerializedName serializedName = (SerializedName) ann;
+                                       if (serializedName.value().equalsIgnoreCase(value)){
+                                           return t;
+                                       }
+                                   }
+                               }
+                           } catch (NoSuchFieldException | SecurityException | IllegalArgumentException ex) {
+                               // Qualquer restrição ignora
                            }
 
                            if(t.name().equalsIgnoreCase(value) || t.toString().equalsIgnoreCase(value)) {
